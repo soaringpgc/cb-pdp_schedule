98985e5b825ccc4b2c2ede1a4299920b4e5e4e7f
Dave Johnson <dave_johnson@mac.com>
2024-03-27 20:12:50 -0400

    Previous fix for the trades layout did not work Screwed up tables in cloudbase I think I got it this time

--- inc/admin/js/templates.js
+++ inc/admin/js/templates.js
@@ -1,17 +1,17 @@
 // Trade Types 
   var tradetypetemplate = _.template(`
-   <div class="Cell0"  id = "id" > <%= id %></div>
-     <div >
-     <label class="Cell2" > <%= trade %></label>
-      <div class="hiding" > <%= role %></div>
+   <div >
+      <div   class="Cell0"  id = "id" > <%= id %></div>
+      <label class="Cell2" > <%= trade %></label>
+      <div   class="hiding" > <%= role %></div>
       <label class="Cell2" > <%= role_label %></label>
       <div class="hiding" > <%= authority %></div>
-     <label class="Cell"><%=  authority_label  %> </label>
-    <div class="hiding" > <%= overrideauthority %></div>
-     <label class="Cell" > <%= overrideauthoritylabel %>  </label>
-     <label class="Cell" > <%= sessionmax %>  </label>
-     <label class="Cell" > <%= yearmin %>  </label>
-     <div class="Cell"> <button class="delete" ">Delete</button></div>
+      <label class="Cell"><%=  authority_label  %> </label>
+      <div class="hiding" > <%= overrideauthority %></div>
+      <label class="Cell" > <%= overrideauthoritylabel %>  </label>
+      <label class="Cell" > <%= sessionmax %>  </label>
+      <label class="Cell" > <%= yearmin %>  </label>
+      <div class="Cell"> <button class="delete" ">Delete</button></div>
    </div>      
 `);
 // 
--- inc/admin/views/html_trade_setup_tab.php
+++ inc/admin/views/html_trade_setup_tab.php
@@ -3,51 +3,57 @@
 	var cb_admin_tab = "trade_types";
 </script>
 
-
 <div style="display:inline-block"  align:left id="trade_types"  class="editform" >
 <?php 			
 if( current_user_can( 'manage_options' ) ) {	
   echo ('   <h3>Trade Types</h3>
-  <DIV id="addtradetypes">   
+
    <form id="addtradetypes" action="#" >
-  <div>    
+   <div>
     	<input type = "hidden"
             id = "id"
             size = "2"
             value = ""
             name = "id"/>
         <div class="hform">   
-        <label for="trade" style=color:black>Trade: </label>                  		   
-        <input type = "text"
-            id = "trade"
-            size = "20"
-            title = "trade ." 
-            name = "trade"/> 
-           </div> 
+       		 <label for="trade" style=color:black>Trade: </label>                  		   
+       		 <input type = "text"
+       		     id = "trade"
+       		     size = "20"
+       		     title = "trade ." 
+       		     name = "trade"/> 
+       	</div> 
 		<div class="hform">	
-        <label for="role" style=color:black>Role: </label>      		
-		<select name ="role" id="role" name="role" >
-			<option value="0"> Select Authority </option>');
-		echo ('</select>  </div>
+        	<label for="role" style=color:black>Role: </label> <br>     		
+			<select name ="role" id="role" name="role" >
+				<option value="0"> Select Role </option>');
+	
+				$value_label_authority = get_option('cloud_base_authoritys');
+				foreach ($value_label_authority  as $key => $authority ){
+					echo ('<option value="' . $key . '">' . $authority . '</option>');
+				}	
+	
+			echo ('</select>  
+		</div>
         <div class="hform">   
-        <label for="authority" style=color:black>Authority: </label>          		
+        <label for="authority" style=color:black>Authority: </label><br>          		
 		<select name ="authority" id="authority" name="authority" >
 			<option value="0"> Select Authority </option>
 			');
 			// authority array is stored in WP options, It is created/updated on activation 
-			$value_label_authority = get_option('cloud_base_authoritys');
+// 			$value_label_authority = get_option('cloud_base_authoritys');
 
 			foreach ($value_label_authority  as $key => $authority ){
 				echo ('<option value="' . $key . '">' . $authority . '</option>');
 			}	
 		echo ('</select>         </div>
         <div class="hform"> 
-        <label for="overrideauthority" style=color:black>Over Ride Authority: </label>       
+        <label for="overrideauthority" style=color:black>Over Ride Authority: </label> <br>      
 		<select name ="overrideauthority" id="overrideauthority"  >
 			<option value="0"> Select Over Ride Authority </option>
 			');
 			// authority array is stored in WP options, It is created/updated on activation 
-			$value_label_authority = get_option('cloud_base_authoritys');
+// 			$value_label_authority = get_option('cloud_base_authoritys');
 
 			foreach ($value_label_authority  as $key => $authority ){
 				echo ('<option value="' . $key . '">' . $authority . '</option>');
@@ -69,41 +75,42 @@ if( current_user_can( 'manage_options' ) ) {
             name = "yearmin"
             min = "0" max = "25"/> 	
         </div>
-        <div class="hform">         	
-        <label for="add" style=color:black>Add</label>      
-        <button id="add" class="view">Submit</button>
+         <br style="clear:both;">
+        <div >         	
+    
+        <button id="add" class="view">Add</button>
         <button id="update" class="cb_edit">Update</button>
-        </div>
-       
-    </form></div>');
+        </div>    
+      </div>   
+    </form>');
 }    
 ?>   
-
-<div class="Table">
-    <div class="Title">
-        <p>Trade Types</p>
-    </div>
-    <div class="Heading">
-        <div class="Cell0"  >
-            <p>ID</p>
-        </div>
-        <div class="Cell2" >
-            <p>Trade</p>
-        </div>
-        <div class="Cell2" >
-            <p>Role</p>
-        </div>
-        <div class="Cell" >
-            <p>Authority</p>
-        </div>
-        <div class="Cell" >
-            <p>Over Ride Authority</p>
-        </div> 
-        <div class="Cell"  >
-            <p>Max per Session</p>
-        </div>  
-        <div class="Cell"  >
-            <p>Min per Year</p>
-        </div>                       
-    </div>
+	<div class="Table">
+	    <div class="Title">
+	        <p>Trade Types</p>
+	    </div>
+	    <div class="Heading">
+	        <div class="Cell0"  >
+	            <p>ID</p>
+	        </div>
+	        <div class="Cell2" >
+	            <p>Trade</p>
+	        </div>
+	        <div class="Cell2" >
+	            <p>Role</p>
+	        </div>
+	        <div class="Cell" >
+	            <p>Authority</p>
+	        </div>
+	        <div class="Cell" >
+	            <p>Over Ride Authority</p>
+	        </div> 
+	        <div class="Cell"  >
+	            <p>Max per Session</p>
+	        </div>  
+	        <div class="Cell"  >
+	            <p>Min per Year</p>
+	        </div>                       
+	    </div>
+	</div>
 </div>
